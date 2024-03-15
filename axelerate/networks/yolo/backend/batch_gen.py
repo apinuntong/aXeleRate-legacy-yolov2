5a056fee22c9bfe10e965c1b84dd727d94c5f6f0
apinuntong <apinun.tong@gmail.com>
2024-03-15 23:20:50 +0700

    Update batch_gen.py

--- axelerate/networks/yolo/backend/batch_gen.py
+++ axelerate/networks/yolo/backend/batch_gen.py
@@ -82,7 +82,7 @@ class BatchGenerator(Sequence):
                 annotation.append(labels[j])
                 annotations.append(annotation)
             anns_list.append(np.array(annotations))
-        return imgs_list, np.array(anns_list,dtype=float32)
+        return imgs_list, np.array(anns_list,dtype=float)
 
 
     def __getitem__(self, idx):
